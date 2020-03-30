fe1b2da014966fb17462ba21d886cf2b16331a70
Peter Kin <kinpeter85@gmail.com>
2020-03-30 19:58:45 +0200

    Add git pull origin script

--- bash-git.sh
+++ bash-git.sh
@@ -7,10 +7,19 @@ function git-co {
   fi
   if [[ $branch == *"remotes/origin"* ]]; then
     local cutName=${branch:17}
+    echo -e "\e[32m[+] Changing to branch: $cutName...\n\e[39m"
     git checkout $cutName
     git pull origin $cutName
-  else 
+  else
+    echo -e "\e[32m[+] Changing to branch: $branch...\n\e[39m"
     git checkout $branch
   fi
+  return 0
+}
+
+function git-pulo {
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+  echo -e "\e[32m[+] Pulling $branch from origin...\n\e[39m"
+  git pull origin $branch
   return 0
 }
