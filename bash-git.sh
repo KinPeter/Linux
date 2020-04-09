cc39d58408b5c0cec11e386deaa6a813285ea372
Peter Kin <kinpeter85@gmail.com>
2020-04-09 07:46:35 +0200

    Add script for git review

--- bash-git.sh
+++ bash-git.sh
@@ -17,6 +17,21 @@ function git-co {
   return 0
 }
 
+function git-review {
+  local branch=$(git branch -a | grep -m 1 $1)
+  if [[ ${#branch} == 0 ]]; then
+    echo -e "\e[31m[-] No such branch\n\e[39m"
+  fi
+  if [[ $branch == *"remotes/origin"* ]]; then
+    local cutName=${branch:10}
+    echo -e "\e[32m[+] Changing to remote branch: $cutName...\n\e[39m"
+    git checkout $cutName
+  else
+    echo -e "\e[31m[-] Branch is checked out locally\n\e[39m"
+  fi
+  return 0
+}
+
 function git-pulo {
   local branch=$(git rev-parse --abbrev-ref HEAD)
   echo -e "\e[32m[+] Pulling $branch from origin...\n\e[39m"
