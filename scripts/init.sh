c282544126b56765475a3f5cc2261faf24618eca
Peter Kin <kinpeter85@gmail.com>
2021-03-15 20:28:57 +0100

    Add ssh and bookmarks

--- scripts/init.sh
+++ scripts/init.sh
@@ -18,5 +18,17 @@ echo "source $(pwd)/bash-git.sh" >> $HOME/.bashrc
 echo "source $(pwd)/bash-others.sh" >> $HOME/.bashrc
 echo -e "\n[+] Custom scripts sourced\n"
 
+# Generate new SSH key
+ssh-keygen -q -f $HOME/.ssh/id_rsa -N ""
+echo -e "\n[+] New SSH key generated:\n"
+cat $HOME/.ssh/id_rsa.pub
+echo -e "\n--------\n"
+
+# Add bookmarks to Nautilus
+echo "sftp://peter@192.168.0.100/home/peter KP-NUC" >> $HOME/.config/gtk-3.0/bookmarks
+echo "sftp://peter@192.168.0.109/home/peter KP-ZENBOOK" >> $HOME/.config/gtk-3.0/bookmarks
+echo "sftp://peter@192.168.0.112/home/peter KP-BRIX" >> $HOME/.config/gtk-3.0/bookmarks
+echo "sftp://pkincom@p-kin.com/home/pkincom P-Kin.com" >> $HOME/.config/gtk-3.0/bookmarks
+
 # Install SNAP apps
 source ./snap-install.sh
